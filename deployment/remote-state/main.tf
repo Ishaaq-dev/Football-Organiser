5cbf576d79613ec63dc763dd6f2d587471825149
Zafar Iqbal <iqbal.zafar74@gmail.com>
2021-12-29 17:09:37 +0000

    MacOS for Dad

--- deployment/remote-state/main.tf
+++ deployment/remote-state/main.tf
@@ -2,28 +2,92 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "development-football-organiser-terraform-state"
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "dad_iam_for_lambda"
 
-  acl = "private"
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
 }
 
-resource "aws_dynamodb_table" "terraform_state_lock" {
-  name           = "development-football-organiser-terraform-state-lock"
-  read_capacity  = 1
-  write_capacity = 1
-  hash_key       = "LockID"
+resource "aws_sns_topic" "user_updates" {
+  name            = "dad-user-updates-topic"
+  delivery_policy = <<EOF
+{
+  "http": {
+    "defaultHealthyRetryPolicy": {
+      "minDelayTarget": 20,
+      "maxDelayTarget": 20,
+      "numRetries": 3,
+      "numMaxDelayRetries": 0,
+      "numNoDelayRetries": 0,
+      "numMinDelayRetries": 0,
+      "backoffFunction": "linear"
+    },
+    "disableSubscriptionOverrides": false,
+    "defaultThrottlePolicy": {
+      "maxReceivesPerSecond": 1
+    }
+  }
+}
+EOF
+}
+
+resource "aws_sqs_queue" "terraform_queue" {
+  name                        = "terraform-example-queue.fifo"
+  fifo_queue                  = true
+  content_based_deduplication = true
+}
+
+
+
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name           = "dadGameScores"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "UserId"
+  range_key      = "GameTitle"
 
   attribute {
-    name = "LockID"
+    name = "UserId"
     type = "S"
+  }
+
+  attribute {
+    name = "GameTitle"
+    type = "S"
+  }
+
+  attribute {
+    name = "TopScore"
+    type = "N"
+  }
+
+  global_secondary_index {
+    name               = "GameTitleIndex"
+    hash_key           = "GameTitle"
+    range_key          = "TopScore"
+    write_capacity     = 10
+    read_capacity      = 10
+    projection_type    = "INCLUDE"
+    non_key_attributes = ["UserId"]
+  }
+
+  tags = {
+    Name        = "dynamodb-table-1"
+    Environment = "production"
   }
 }
