a83ed98bd9d41757f2b97d120231fcca50a04b56
Is'haaq Iqbal <ishaaq.iqbal@naimuri.com>
2021-12-30 16:30:41 +0000

    adds lambda src code to project

--- deployment/football-organiser/main.tf
+++ deployment/football-organiser/main.tf
@@ -7,6 +7,7 @@
 
 locals {
   incoming_lambda_name = "${var.environment}-${var.project}-handle-incoming-sms"
+  lambda_handler       = "LambdaHandler.process_event"
 }
 
 data "archive_file" "incoming_lambda_zip" {
@@ -19,7 +20,7 @@ resource "aws_lambda_function" "incoming_lambda" {
   filename         = "src-zip/incoming-lambda.zip"
   function_name    = local.incoming_lambda_name
   role             = "arn:aws:iam::574674178402:role/iam_for_lambda"
-  handler          = "lambda_function.lambda_handler"
+  handler          = local.lambda_handler
   source_code_hash = filebase64sha256("src-zip/incoming-lambda.zip")
   runtime          = "python3.9"
 }
