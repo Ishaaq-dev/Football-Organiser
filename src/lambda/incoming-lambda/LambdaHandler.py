2dda5b843c31765d373b967adf627f9ef66d0dd2
Is'haaq Iqbal <ishaaq.iqbal@naimuri.com>
2021-12-30 15:50:42 +0000

    adds lambda src code to project

--- src/lambda/incoming-lambda/LambdaHandler.py
+++ src/lambda/incoming-lambda/LambdaHandler.py
@@ -1,13 +1,14 @@
 import os
 import json
 import boto3 as aws
+from incoming import handle_incoming_message
 
 
 def process_event(event, context):
     body = event['Records'][0]['body']
-    message = json.loads(body)
-    print(message['Message'])
+    body_json = json.loads(body)
+    handle_incoming_message(body_json, context)
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': body_json
     }
---
+++ src/lambda/incoming-lambda/incoming.py
@@ -0,0 +1,7 @@
+import os
+import json
+
+
+def handle_incoming_message(body_json, context):
+    print(body_json)
+    print(context)
