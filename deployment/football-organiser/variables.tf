de9a3eddfc8bbd7b8c37e63c2f1eb1df74ce6da9
Is'haaq Iqbal <ishaaq.iqbal@naimuri.com>
2021-12-30 13:52:06 +0000

    adds lambda src

--- deployment/football-organiser/variables.tf
+++ deployment/football-organiser/variables.tf
@@ -1,10 +1,11 @@
 variable "environment" {
   type        = string
   description = "A string representing the environment you would like to deploy in"
-  default     = "development"
+  default     = "dev"
 }
 
 variable "project" {
   type        = string
   description = "A string representing the name of the project"
+  default     = "football-organiser"
 }
