9eab26c90a02c29a2f27d13f3b815a1e80f00a4c
Ishaaq Iqbal <i.iqbalwork@gmail.com>
2022-06-27 18:27:38 +0100

    creates sns sqs module

--- deployment/main.tf
+++ deployment/main.tf
@@ -1,20 +1,17 @@
 # need to create the following:
-# - Lambda
-# - sns
-# - sqs
-# - pinpoint
-# - storage (DynamoDB/ElasticSearch)
+# - Lambda - done
+# - sns - done
+# - sqs - done
+# - pinpoint - create resource then import remote config
+# - storage (DynamoDB/ElasticSearch) - done
 
 locals {
   contacts_dynamo_name         = "${var.prefix}-${var.project}-contacts"
-
-  # Incoming Lambda
-  enquire_incoming_sns_name    = "${var.prefix}-${var.project}-enquire-incoming-sms-sns"
-  enquire_incoming_message_sqs = "${var.prefix}-${var.project}-enquire-incoming-sms-sqs"
 }
 
 module "incoming_lambda_function" {
   source = "./modules/lambda"
+
   prefix = var.prefix
   project = var.project
   lambda_name = "incoming-lambda"
@@ -23,48 +20,13 @@ module "incoming_lambda_function" {
   }
 }
 
-resource "aws_sns_topic" "enquire_incoming_message_sns" {
-  name = local.enquire_incoming_sns_name
-}
+module "incoming_sns_sqs" {
+  source = "./modules/sns-sqs"
 
-resource "aws_sqs_queue" "enquire_incoming_message_sqs" {
-  name = local.enquire_incoming_message_sqs
-}
-
-resource "aws_sqs_queue_policy" "test" {
-  queue_url = aws_sqs_queue.enquire_incoming_message_sqs.id
-
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Id": "sqspolicy",
-  "Statement": [
-    {
-      "Sid": "First",
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": "sqs:SendMessage",
-      "Resource": "${aws_sqs_queue.enquire_incoming_message_sqs.arn}",
-      "Condition": {
-        "ArnEquals": {
-          "aws:SourceArn": "${aws_sns_topic.enquire_incoming_message_sns.arn}"
-        }
-      }
-    }
-  ]
-}
-POLICY
-}
-
-resource "aws_sns_topic_subscription" "enquire_incoming_sns_sqs_subscription" {
-  topic_arn = aws_sns_topic.enquire_incoming_message_sns.arn
-  protocol  = "sqs"
-  endpoint  = aws_sqs_queue.enquire_incoming_message_sqs.arn
-}
-
-resource "aws_lambda_event_source_mapping" "example" {
-  event_source_arn = aws_sqs_queue.enquire_incoming_message_sqs.arn
-  function_name    = module.incoming_lambda_function.lambda_arn
+  prefix = var.prefix
+  project = var.project
+  name = "enquire-incoming-sms"
+  lambda_arn = module.incoming_lambda_function.lambda_arn
 }
 
 resource "aws_dynamodb_table" "contacts_dynamodb" {
---
+++ deployment/modules/sns-sqs/variables.tf
@@ -0,0 +1,15 @@
+variable "prefix" {
+  type = string
+}
+
+variable "project" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
+variable "lambda_arn" {
+  type = string
+}
