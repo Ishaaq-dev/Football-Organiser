5cb1e07ac1e77f22c6126905c76c374386a3e8c9
Ishaaq Iqbal <i.iqbalwork@gmail.com>
2022-06-28 10:09:35 +0100

    removes comments

--- src/lambda/contact-players/contact_players.py
+++ src/lambda/contact-players/contact_players.py
@@ -6,10 +6,10 @@ dynamoDB = aws.client('dynamodb')
 contact_table_name = os.environ['contacts_table']
 
 def handle_contacting_players(body_json):
+    # I would like to create a utils layer that handles dynamodb requests
     phone_numbers = dynamoDB.scan(
         TableName=contact_table_name,
         Select='SPECIFIC_ATTRIBUTES',
         ProjectionExpression='phone_number'
-    ).get('Items')
-    # phone_numbers = [ player.get('phone_number') for player in players ]
+    )
     print('phone numbers: {}'.format(phone_numbers))
