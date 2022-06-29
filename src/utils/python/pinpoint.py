ea60eb5105d8c05729346270e41a242056ca94bf
Ishaaq Iqbal <i.iqbalwork@gmail.com>
2022-06-29 14:37:17 +0100

    merges with master

--- src/utils/python/pinpoint.py
+++ src/utils/python/pinpoint.py
@@ -3,13 +3,9 @@ import boto3 as aws
 pinpoint = aws.client('pinpoint')
 
 MESSAGE_TYPE = ('TRANSACTIONAL', 'PROMOTIONAL')
-<<<<<<< HEAD
 MESSAGES = {
     'initial': 'Would you like to play football this Saturday: \n\n-> Accrington Stanley \n-> Thorneyholme Rd BB5 6BD \n\n-> 14:00-15:00 \n\n-> £3.50 \n\nPlease reply: \nyes \nno'
 }
-=======
-
->>>>>>> master
 def send_messages(phone_numbers, message):
     addresses = {}
     for phone_number in phone_numbers:
@@ -21,11 +17,7 @@ def send_messages(phone_numbers, message):
             'Addresses': addresses,
             'MessageConfiguration': {
                 'SMSMessage': {
-<<<<<<< HEAD
                     'Body': MESSAGES.get(message),
-=======
-                    'Body': message,
->>>>>>> master
                     'MessageType': MESSAGE_TYPE[1]
                 }
             }
