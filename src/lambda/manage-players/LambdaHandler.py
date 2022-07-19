9cd25dd10517856539bf062465398fd086ece458
Ishaaq Iqbal <i.iqbalwork@gmail.com>
2022-07-19 22:37:58 +0100

    removes print statements in lambdaHandler

--- src/lambda/manage-players/LambdaHandler.py
+++ src/lambda/manage-players/LambdaHandler.py
@@ -1,5 +1,7 @@
+from manage_players import handle_manage_players
 def process_event(event, context):
+    response = handle_manage_players(event)
     return {
         'statusCode': 200,
-        'body': event
+        'body': response
     }
