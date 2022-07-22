11f6afcfd984d6ddd2bdcded539fdd343e557608
Ishaaq Iqbal <i.iqbalwork@gmail.com>
2022-07-23 00:37:24 +0100

    creates one_time terraform config for RDS

---
+++ deployment/one_time/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.60.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "personal-football-organiser-terraform-state"
+    region         = "eu-west-1"
+    dynamodb_table = "personal-football-organiser-terraform-state-lock"
+    encrypt        = true
+    key            = "one_time/terraform.tfstate"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
